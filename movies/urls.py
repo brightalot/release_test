692bfc89b4725d85fe51b23ef03d25e410b20de9
brightalot <gamja9151@hanyang.ac.kr>
2024-07-30 19:23:39 +0900

    update requirements.txt and delete .pyc files

--- movies/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# /movies/title   GET POST
-
-urlpatterns = [
-    path("", views.Movies.as_view()),
-    path("<str:title>/", views.MoviesDetail.as_view(), name='movie-detail'),
-]
--- students/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# /students/code   GET POST
-
-urlpatterns = [
-    path("", views.Students.as_view()),
-    path("<str:code>/", views.StudentDetail.as_view())
-]
