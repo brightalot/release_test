ad8d6a98287860efdf668758600d3465908d58fc
brightalot <gamja9151@hanyang.ac.kr>
2024-07-31 18:57:50 +0900

    asgi 수정 및 daphne 호환성 문제 해결

--- config/asgi.py
+++ config/asgi.py
@@ -4,7 +4,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'release_test.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
